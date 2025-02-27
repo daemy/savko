5b433b10ae02bf4901878dad32fe5187a318a98b
daemy <ilioskidimitar98@gmail.com>
2025-02-27 19:53:20 +0100

    working version backend rust clipboard manager + connect with svelte

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,91 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs::{remove_file, File, OpenOptions};
+use std::io::{BufReader, BufWriter};
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+use arboard::Clipboard;
+use serde::{Deserialize, Serialize};
+use tauri::ipc::Channel;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Notification {
+    message: String,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ClipboardHistory {
+    items: Vec<Notification>,
+}
+
+const PATH : &str = "data/clipboard_history.json";
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn wipe_all() {
+    let _ = remove_file(PATH);
+}
+
+#[tauri::command]
+fn copy(data: String) {
+    let mut clipboard = Clipboard::new().unwrap();
+    clipboard.set_text(data).unwrap()
+}
+
+#[tauri::command]
+fn load_last_n_entries(n: usize) -> Vec<Notification> {
+    if let Ok(history) = load_history() {
+        history.items.into_iter().rev().take(n).collect()
+    } else {
+        vec![]
+    }
+}
+
+#[tauri::command]
+fn init(on_event : Channel<String>) {
+    spawn(move || {
+        let mut clipboard = Clipboard::new().unwrap();
+        let mut last_clipboard: Option<String> = None;
+
+        loop {
+            if let Ok(data) = clipboard.get_text() {
+                if last_clipboard.as_ref() != Some(&data) {
+                    last_clipboard = Some(data.clone());
+                    println!("{}", data.clone());
+                    let mut history = load_history().unwrap_or_else(|_| ClipboardHistory { items: vec![] });
+                    history.items.push(Notification { message: data.clone(), description: "Copied to clipboard".to_string() });
+                    save_history(&history).unwrap();
+                    on_event.send(data).unwrap()
+                }
+            }
+            sleep(Duration::from_secs(1))
+        }
+    });
+}
+
+fn load_history() -> Result<ClipboardHistory, std::io::Error> {
+    let file = File::open(PATH)?;
+    let reader = BufReader::new(file);
+    let history = serde_json::from_reader(reader)?;
+    Ok(history)
+}
+
+fn save_history(history: &ClipboardHistory) -> Result<(), std::io::Error> {
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(PATH)?;
+
+    let writer = BufWriter::new(file);
+
+    serde_json::to_writer_pretty(writer, history)?;
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![wipe_all, copy, load_last_n_entries, init])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,156 +1,97 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+    import { invoke, Channel } from "@tauri-apps/api/core";
+    import { onMount } from "svelte";
+    import { toast } from "svelte-sonner";
+    import Check from "lucide-svelte/icons/check";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
+    import * as Accordion from "$lib/components/ui/accordion/index.js";
 
-  let name = $state("");
-  let greetMsg = $state("");
+    let notifs: { message: string; description: string }[] = [];
+    let closeDialogOpen : boolean = false;
+    const LOAD_COUNT = 5;
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+    async function deleteAll() {
+        await invoke("wipe_all");
+        notifs = [];
+        closeDialogOpen = false;
+    }
+
+    async function copy() {
+        toast.success("Copied to clipboard.", {
+            // this is hardcoded date for now for testing UI, I will provide date from Rust later
+            description: "",
+            action: {
+                label: "Dismiss",
+                onClick: () => console.info("Undo")
+            }
+        })
+    }
+
+    async function load_last_n() {
+        try {
+            notifs = await invoke("load_last_n_entries", { n: LOAD_COUNT });
+        } catch (error) {
+            console.error("Error loading clipboard history:", error);
+        }
+    }
+
+    onMount(async () => {
+        await load_last_n();
+        // Create a new Channel instance
+        const onEvent = new Channel<string>();
+        onEvent.onmessage = (message: string) => {
+            console.log("Clipboard updated:", message);
+            // Append new clipboard text into our notifications array
+            notifs = [{ message, description: "Copied to clipboard" }, ...notifs];
+        };
+
+        try {
+            await invoke("init", { onEvent });
+        } catch (error) {
+            console.error("Error initializing clipboard listener:", error);
+        }
+    });
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
-</main>
-
-<style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
-
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-
-</style>
+<Card.Root class="w-full">
+    <Card.Header>
+        <Card.Title>Clipboard History</Card.Title>
+        <Card.Description>You have {notifs.length} saved entries.</Card.Description>
+    </Card.Header>
+    <Card.Content class="grid gap-4">
+        <Accordion.Root type="single" class="w-full sm:max-w-[70%]">
+        {#each notifs as notification, idx}
+                <Accordion.Item>
+                    <Accordion.Trigger>{notification.message.slice(0, 20)}</Accordion.Trigger>
+                    <Accordion.Content onclick={copy}
+                    >{notification.message}</Accordion.Content
+                    >
+                </Accordion.Item>
+        {/each}
+        </Accordion.Root>
+    </Card.Content>
+    <AlertDialog.Root bind:open={closeDialogOpen}>
+        <AlertDialog.Trigger onclick={() => closeDialogOpen = true}>
+            <Card.Footer class="bottom-0">
+                <Button class="w-full bottom-0">
+                    <Check /> Delete all
+                </Button>
+            </Card.Footer>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+            <AlertDialog.Header>
+                <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+                <AlertDialog.Description>
+                    This action cannot be undone. This will permanently delete your saved clipboards
+                    and remove your data from your local computer.
+                </AlertDialog.Description>
+            </AlertDialog.Header>
+            <AlertDialog.Footer>
+                <AlertDialog.Cancel oncancel={() => closeDialogOpen = false}>Cancel</AlertDialog.Cancel>
+                <AlertDialog.Action onclick={deleteAll}>Continue</AlertDialog.Action>
+            </AlertDialog.Footer>
+        </AlertDialog.Content>
+    </AlertDialog.Root>
+</Card.Root>
